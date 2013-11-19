2bd4829c6f3e35d8d1e771a00421719ad4ba5d2e
Noah Collins <noah.collins@gmail.com>
2013-11-19 09:29:29 -0800

    revised comments

--- app/config.js
+++ app/config.js
@@ -1,29 +1,26 @@
-// This is the runtime configuration file.  It complements the Gruntfile.js by
+// This is the runtime configuration file. It complements the Gruntfile.js by
 // supplementing shared properties.
 require.config({
   paths: {
-    // Make vendor easier to access.
+    // Map vendor dependencies for easy access.
     "vendor": "../vendor",
 
-    // Almond is used to lighten the output filesize.
+    // Almond lightens the output filesize.
     "almond": "../vendor/bower/almond/almond",
 
-    // Opt for Lo-Dash Underscore compatibility build over Underscore.
+    // Lo-Dash Underscore compatibility build instead of underscore.
     "underscore": "../vendor/bower/lodash/dist/lodash.underscore",
-
-    // Map remaining vendor dependencies.
     "jquery": "../vendor/bower/jquery/jquery",
     "backbone": "../vendor/bower/backbone/backbone"
   },
 
   shim: {
-    // This is required to ensure Backbone works as expected within the AMD
-    // environment.
+    // Ensures Backbone works as expected within the AMD environment.
     "backbone": {
-      // These are the two hard dependencies that will be loaded first.
+      // The two hard dependencies that will be loaded first.
       deps: ["jquery", "underscore"],
 
-      // This maps the global `Backbone` object to `require("backbone")`.
+      // Map the global `Backbone` object to `require("backbone")`.
       exports: "Backbone"
     }
   }
